c6591afe6d857da6d3a9df718a2265e00c574527
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2022-04-23 22:10:33 +0200

    Handle bin file

---
+++ bin/wesp.js
@@ -0,0 +1,18 @@
+const {execSync} = require('child_process');
+
+/* TODO
+if [ ! -e "./wesp.js" ]; then
+    echo "No wesp.js file found in $(pwd)"
+    exit 1
+fi
+*/
+
+let exitCode = 0;
+while (exitCode !== 3) {
+  try {
+    execSync('node ./wesp.js');
+  } catch (error) {
+    exitCode = error.status; // Might be 127 in your example.
+  }
+}
+process.exit(exitCode);
